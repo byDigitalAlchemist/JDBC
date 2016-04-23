8422521531ae64a8401d0e74ee2f4bbdff169a47
Digital Alchemist <developer.anwar@gmail.com>
2016-04-23 13:27:56 -0400

    Jdbc Project updated on Github
    Connecting with mysql and working

---
+++ Jdbc-Basic/src/main/java/com/dc/database/jdbc/basic/Application.java
@@ -0,0 +1,66 @@
+package com.dc.database.jdbc.basic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application {
+	public static void main(String[] args) {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+
+		System.out.println(" Connecting ...");
+		
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rockstardb", "root", "webmaster.ubit");
+
+			System.out.println("Database connection successful!\n");
+
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+
+			// 3. Execute SQL query
+			myRs = myStmt.executeQuery("select * from employees");
+
+			// 4. Process the result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+			}
+		} catch (Exception exc) {
+			
+			System.out.println();
+			exc.printStackTrace();
+		} finally {
+			
+			try{
+			
+				if (myRs != null) {
+					myRs.close();
+				}
+
+				if (myStmt != null) {
+					myStmt.close();
+				}
+
+				if (myConn != null) {
+					myConn.close();
+				}
+			}
+			catch(SQLException exception){
+				exception.printStackTrace();
+			}
+			
+			
+		}
+
+	}
+}
